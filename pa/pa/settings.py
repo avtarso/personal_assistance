0aa010b4e9e65e53934d936e4d062e1d5890f5a6
Avtarso <t0676352927@gmail.com>
2024-06-20 15:27:59 +0300

    fix settings for static files

--- pa/pa/settings.py
+++ pa/pa/settings.py
@@ -142,11 +142,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# (удалить перед развертыванием в продакшн)
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
