aa18415eb02b5c36b3701142ddf42b17669956f2
Oleh Davydenko <oleg.davidenko@gmail.com>
2024-06-30 21:05:16 +0300

    Add news random quotes show api

---
+++ pa/pa/api.py
@@ -0,0 +1,54 @@
+from random import choice
+
+from tastypie.authentication import BasicAuthentication
+from tastypie.authorization import Authorization
+from tastypie.exceptions import Unauthorized
+from tastypie.resources import ModelResource
+
+from news.models import NewsQuote
+
+
+class UserObjectsOnlyAuthorization(Authorization):
+    def read_list(self, object_list, bundle):
+        return object_list.filter(added_by=bundle.request.user)
+
+    def read_detail(self, object_list, bundle):
+        return bundle.obj.added_by == bundle.request.user
+
+    def create_list(self, object_list, bundle):
+        raise Unauthorized("Unauthorized!")
+
+    def create_detail(self, object_list, bundle):
+        raise Unauthorized("Unauthorized!")
+
+    def update_list(self, object_list, bundle):
+        raise Unauthorized("Unauthorized!")
+
+    def update_detail(self, object_list, bundle):
+        raise Unauthorized("Unauthorized!")
+
+    def delete_list(self, object_list, bundle):
+        raise Unauthorized("Unauthorized!")
+
+    def delete_detail(self, object_list, bundle):
+        raise Unauthorized("Unauthorized!")
+
+
+class NewsQuoteResource(ModelResource):
+    class Meta:
+        queryset = NewsQuote.objects.all()
+        resource_name = "news_quote"
+        excludes = ["id"]
+
+        authentication = BasicAuthentication()
+        authorization = UserObjectsOnlyAuthorization()
+
+    def authorized_read_list(self, object_list, bundle):
+
+        ids = object_list.filter(added_by=bundle.request.user).values_list(
+            "id", flat=True
+        )
+
+        id = choice(ids)
+
+        return object_list.filter(added_by=bundle.request.user, id=id)
