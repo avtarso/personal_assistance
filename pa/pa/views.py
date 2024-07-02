6bd9e6547adf3882cb18fab71eef41972417de17
Avtarso <t0676352927@gmail.com>
2024-07-03 02:20:15 +0300

    migrate 404 page to root directory, fix text

--- pa/pa/views.py
+++ pa/pa/views.py
@@ -6,6 +6,6 @@ def random_quote_api(request):
     return render(request, "quotes/random_quote_api.html")
 
 def handler404(request, exception):
-    return render(request, 'quotes/404.html', status=404)
+    return render(request, '404.html', status=404)
 
 
--- pa/views/error_views.py
+++ pa/views/error_views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def handler404(request, exception):
-    return render(request, 'quotes/404.html', status=404)
+    return render(request, '404.html', status=404)
